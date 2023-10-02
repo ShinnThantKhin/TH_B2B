5d2149acc383273dd363f2959f9884152584db60
Shinn Thant Khin <99662133+ShinnThantKhin@users.noreply.github.com>
2023-10-02 16:02:49 +0630

    Update calculator_streamlit.py

--- calculator_streamlit.py
+++ calculator_streamlit.py
@@ -4,12 +4,17 @@ st.title('TH Telemedic B2B Calculator')
 
 st.subheader('TH Fees - 10%')
 
-doctor = st.number_input('Doctor Fees',step=1, value=0, format="%d")
-agent = st.number_input('Agent Fees', step=1, value=0, format="%d")
+doctor_text = st.text_input('Doctor Fees')
+agent_text = st.text_input('Agent Fees')
 
 btn = st.button('Calculate')
 
 if btn:
+    try:
+        doctor = int(doctor_text)
+        agent = int(agent_text)
+    except ValueError:
+        st.warning("Please enter valid integer values for Doctor Fees and Agent Fees.")
     subtotal = doctor+agent
     th_fees = subtotal/10
     alltotal = subtotal + th_fees
@@ -17,11 +22,10 @@ if btn:
     formatted_sub = "{:.0f}".format(subtotal)
     formatted_th = "{:.0f}".format(th_fees)
     formatted_all= "{:.0f}".format(alltotal)
-    formatted_x = "{:.2f}".format(x)
     st.success(f'Subtotal Fees : {formatted_sub} kyats')
     st.success(f'TH Fees : {formatted_th} kyats')
     st.success(f'Total Fees : {formatted_all} kyats')
     st.text('Onboard Fees')
-    st.success(f'Onboard Fees : {formatted_x} kyats')
+    st.success(f'Onboard Fees : {x} kyats')
     
   
